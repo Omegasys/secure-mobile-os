df6291e2290fd233e5589b0f2d7b365e6d0f174f
Omega <emomega@proton.me>
2026-06-07 23:04:45 -0400

    Create kernel_tests.rs

---
+++ tests/kernel_tests.rs
@@ -0,0 +1,11 @@
+#[test]
+fn test_kernel_boot() {
+    let boot_success = true;
+    assert!(boot_success);
+}
+
+#[test]
+fn test_memory_init() {
+    let memory_ok = true;
+    assert!(memory_ok);
+}
