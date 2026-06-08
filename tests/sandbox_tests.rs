5a1ade34975d9d8781c20f61b95760d84fe40b0e
Omega <emomega@proton.me>
2026-06-07 23:05:07 -0400

    Create sandbox_tests.rs

---
+++ tests/sandbox_tests.rs
@@ -0,0 +1,11 @@
+#[test]
+fn test_app_isolation() {
+    let isolated = true;
+    assert!(isolated);
+}
+
+#[test]
+fn test_memory_limit() {
+    let within_limit = true;
+    assert!(within_limit);
+}
